9313b4671af918a1b1f0473ecf1c85e97d72cec8
Paul Hellings <info@paulhellings.nl>
2023-04-24 21:37:27 +0200

    Add megamix page and router

---
+++ src/components/PaulMariekeFestivalMegamix.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="row mt-4">
+    <div class="col-sm">
+    </div>
+    <div class="col-sm">
+      <iframe width="400" height="400" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1499270632%3Fsecret_token%3Ds-BLSVhrzkKue&color=%235c8a80&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"></div>
+      <a href="https://www.dropbox.com/s/4sdiw3vhx17z6dn/Paul%20%26%20Marieke%20-%20Festival%20Megamix.mp3?dl=0">Download</a>
+    </div>
+    <div class="col-sm">
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PaulMariekeFestivalMegamix.vue',
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,24 @@
-import Vue from 'vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 import App from './App.vue'
 import VueYouTubeEmbed from 'vue-youtube-embed'
+import PaulMariekeFestivalMegamix from '@/components/PaulMariekeFestivalMegamix.vue';
+import home from '@/components/Home.vue';
 Vue.use(VueYouTubeEmbed)
+Vue.use(VueRouter)
 
 require('./css/styles.css')
 
 Vue.config.productionTip = false
 
+const routes = [
+  { path: '/', component: home },
+  { path: '/megamix', component: PaulMariekeFestivalMegamix }
+]
+
+const router = new VueRouter({routes})
+
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
