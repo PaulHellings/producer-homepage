7576a715e459c1e78cee34bc2d108b97611da40e
Paul Hellings <info@paulhellings.nl>
2025-03-31 21:40:31 +0200

    Add hosting

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB0bXBd--WsV3cDOsMoDC13iilwRMUb1pg",
+  authDomain: "paul-hellings.firebaseapp.com",
+  projectId: "paul-hellings",
+  storageBucket: "paul-hellings.firebasestorage.app",
+  messagingSenderId: "473689204806",
+  appId: "1:473689204806:web:1267957d661afac3a3c247"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
